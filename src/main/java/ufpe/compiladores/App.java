1fc159882967c9dfe856a7414ef9b713437f2120
Nara Souza <nsa2@cin.ufpe.br>
2019-02-24 17:49:57 -0300

    Exercício: infix expressions to postfix expressions

---
+++ src/main/java/ufpe/compiladores/App.java
@@ -0,0 +1,32 @@
+package ufpe.compiladores;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+/**
+ * Infix expressions to postfix expressions
+ */
+public final class App {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String input = in.nextLine();
+        StringBuilder output = new StringBuilder();
+        Stack<Character> operations = new Stack<>();
+
+        for(int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+            if(!((c == '+') || (c == '-'))) {
+                output.append(c);
+            } else {
+                operations.push(c);
+            }
+        }
+        while(!operations.empty()) {
+            output.append(operations.pop());
+        }
+
+        System.out.println(output);        
+
+        in.close();
+    }
+}
